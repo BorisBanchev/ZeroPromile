48a645a7a705ea59cd2544f87a0b86303df45f4f
BorisBanchev <boris.banchev03@gmail.com>
2026-01-15 17:10:40 +0200

    Change structure and rename files

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config({
   plugins: {
     "@stylistic": stylistic,
   },
-  ignores: ["dist/*"],
+  ignores: ["dist/*", "prisma.config.ts"],
   rules: {
     "@stylistic/semi": "error",
     "@typescript-eslint/no-unsafe-assignment": "error",
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import usersRouter from "./routes/users";
 
 const port: number = Number(process.env.PORT) || 3000;
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
