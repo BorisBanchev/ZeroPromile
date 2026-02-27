40919a629e3a0e6ae0a3a3eec6f8dc17954c728b
BorisBanchev <boris.banchev03@gmail.com>
2026-02-27 17:04:14 +0200

    Fix CI pipeline

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
         ? process.env.DATABASE_URL
         : process.env.NODE_ENV === "test"
           ? process.env.TEST_DATABASE_URL
-          : process.env.DEV_DATABASE_URL,
+          : process.env.NODE_ENV === "development"
+            ? process.env.DEV_DATABASE_URL
+            : process.env.DATABASE_URL,
   },
 });
