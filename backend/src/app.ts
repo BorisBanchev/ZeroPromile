40d3e99c946561cd8df5df800326198e7c528623
BorisBanchev <boris.banchev03@gmail.com>
2026-03-06 11:48:38 +0200

    Add rate limiting middleware to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
 
 // import routes
 import authRoutes from "./routes/authRoutes";
 import updateProfileRoutes from "./routes/updateProfileRoutes";
 import drinksRoutes from "./routes/drinksRoutes";
-
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 100,
+  message: { error: "Too many requests, please try again later" },
+});
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
 app.use("/api/auth", authRoutes);
 app.use("/api/update/profile", updateProfileRoutes);
 app.use("/api/sessions", drinksRoutes);
