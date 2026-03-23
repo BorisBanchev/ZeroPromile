55d02ff2a69b0dfb26c6547f98e036aeac75e3f4
BorisBanchev <boris.banchev03@gmail.com>
2026-03-23 12:42:49 +0200

    Fix authorization bug where user doesn't get new accesstoken after 15min

--- backend/src/middlewares/errorMiddleware.ts
+++ backend/src/middlewares/errorMiddleware.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../error/appError";
-import { JsonWebTokenError } from "jsonwebtoken";
+import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 export const errorMiddleware = (
   error: unknown,
@@ -11,12 +11,14 @@ export const errorMiddleware = (
 ) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ error: error.message });
+  } else if (error instanceof TokenExpiredError) {
+    return res.status(401).json({ error: "jwt expired" });
+  } else if (error instanceof JsonWebTokenError) {
+    return res.status(401).json({ error: "Not authorized, token failed" });
   } else if (error instanceof z.ZodError) {
     return res.status(400).json({ error: error.issues[0].message });
   } else if (error instanceof Error) {
     return res.status(500).json({ error: error.message });
-  } else if (error instanceof JsonWebTokenError) {
-    return res.status(401).json({ error: "Not authorized, token failed" });
   } else {
     return res.status(500).json({ error: "Internal server error" });
   }
