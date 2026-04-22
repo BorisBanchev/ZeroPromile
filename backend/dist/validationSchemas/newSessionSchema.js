3163fbddb7a7d01fdb44b2619716b38027084bb9
BorisBanchev <boris.banchev03@gmail.com>
2026-04-22 20:34:43 +0300

    Fix CI/CD

--- backend/dist/validationSchemas/newSessionSchema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newSessionSchema = void 0;
-const zod_1 = require("zod");
-exports.newSessionSchema = zod_1.z.object({
-    sessionName: zod_1.z.string().min(1, "Session name is required"),
-});
--- backend/src/routes/updateProfileRoutes.ts
+++ backend/src/routes/updateProfileRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { setGenderAndWeight } from "../controllers/updateUserProfileController";
-import { newUpdateProfileParser } from "../middlewares/newUpdateProfileMiddleware.ts";
+import { newUpdateProfileParser } from "../middlewares/newUpdateProfileMiddleware";
 import { errorMiddleware } from "../middlewares/errorMiddleware";
 
 const router = express.Router();
